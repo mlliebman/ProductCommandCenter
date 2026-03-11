738d958ef257090ac0380241f40710ef4861713d
mlliebman <marclliebman@gmail.com>
2026-03-11 16:38:47 +0000

    fix duplicate base path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({ base: '/ProductCommandCenter/',
+export default defineConfig({
   plugins: [react()],
-  // Change 'product-command' to your actual GitHub repo name
   base: '/ProductCommandCenter/',
   build: {
     outDir: 'dist',
